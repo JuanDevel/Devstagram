cd27b6b9edf549cba904bcf13250c52a030482fb
Juan Diego Cañaveral <juandicanu@gmail.com>
2023-03-22 17:53:11 -0500

    feat: registration form added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,55 @@
 @extends('layouts.app')
 
 @section('title')
-    Sign up for Devstagram
+    Sign up
+@endsection
+
+@section('content')
+    <div class="md:flex md:justify-center md:gap-7 md: items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{asset('img/registrar.jpg')}}" alt="img register">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        name
+                    </label>
+                    <input id="name" type="text" name="name" placeholder="Name" class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        username
+                    </label>
+                    <input id="username" type="text" name="username" placeholder="UserName" class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        email
+                    </label>
+                    <input id="email" type="email" name="email" placeholder="Email" class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        password
+                    </label>
+                    <input id="password" type="password" name="password" placeholder="Password" class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        password confirmation
+                    </label>
+                    <input id="password_confirmation" type="password" name="password_confirmation" placeholder="Password Confirmation" class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <input type="submit" value="Sign up" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                />
+            </form>
+        </div>
+    </div>
 @endsection

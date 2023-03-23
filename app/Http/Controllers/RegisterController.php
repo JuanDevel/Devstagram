6c2d6afd44596abd2679b9790cc54f1ebf87a496
Juan Diego Cañaveral <juandicanu@gmail.com>
2023-03-23 12:07:28 -0500

    feat:password confirmation validation is added, new migration to add username to the users table

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -19,7 +19,10 @@ class RegisterController extends Controller
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
             'email' => 'required|unique:users|email|max:60',
-            'password' => 'required|min:8|max:30',
+            'password' => 'required|min:8|max:30|confirmed',
         ]);
+
+        dd('creando usuario');
     }
+
 }

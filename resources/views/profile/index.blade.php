740cfef6e9bafcc434350906d3f6bca72c3646f8
Juan Diego Cañaveral <juandicanu@gmail.com>
2023-03-30 14:38:36 -0500

    feat: edit profile

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title')
+    Edit Profile: {{auth()->user()->name}}
+@endsection
+
+@section('content')
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-white shadow p-6">
+            <form class="mt-10 md:mt-0" action="">
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input id="name" type="text" name="username" placeholder="Userame" class="border p-3 w-full rounded-lg @error('username')
+                        border-red-500
+                    @enderror" value="{{auth()->user()->username}}"/>
+                    @error('username')
+                        <p class="bg bg-red-500 text-white my-2 rounded-lg text-sm p-2  text-center uppercase">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="image" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Image Profile
+                    </label>
+                    <input id="name" type="file" accept=".jpg, .jpeg, .png" name="image" class="border p-3 w-full rounded-lg @error('image')
+                        border-red-500
+                    @enderror"/>
+                    @error('image')
+                        <p class="bg bg-red-500 text-white my-2 rounded-lg text-sm p-2  text-center uppercase">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <input type="submit" value="save changes" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                />
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 
 /*
@@ -32,18 +33,30 @@ Route::post('/login',[LoginController::class,'store']);
 
 Route::post('/logout',[LogoutController::class,'store'])->name('logout');
 
-Route::get('/{user:username}',[PostController::class,'index'])->name('posts.index');
-Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+// rutas perfil
+
+Route::get('/edit-profile',[ProfileController::class,'index'])->name('profile.index');
+Route::post('/edit-profile',[ProfileController::class,'store'])->name('profile.store');
+
+
+// rutas image
+Route::post('/images',[ImagenController::class,'store'])->name('images.store');
+
+
 Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+Route::get('/{user:username}',[PostController::class,'index'])->name('posts.index');
 Route::get('/{user:username}/posts/{post}',[PostController::class,'show'])->name('posts.show');
 
 Route::post('/{user:username}/posts/{post}',[CommentController::class,'store'])->name('comment.store');
 Route::delete('/posts/{post}',[PostController::class,'destroy'])->name('posts.destroy');
 
-Route::post('/images',[ImagenController::class,'store'])->name('images.store');
+
 
 // like fotos
 
 Route::post('/posts/{post}/likes', [LikeController::class,'store'])->name('posts.likes.store');
 Route::delete('/posts/{post}/likes', [LikeController::class,'destroy'])->name('posts.likes.destroy');
+
+
 
